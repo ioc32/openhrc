112146c375b00f2ece2b727e68f4f6bf416d7628
Saúl Ibarra Corretgé <saghul@gmail.com>
2015-04-25 02:09:22 +0200

    Add extra prints in configure.sh

--- configure.sh
+++ configure.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+echo "Running stage1..."
 ansible-playbook stage1.yml
+
+echo "Running stage2..."
 ansible-playbook stage2.yml
 
 echo "System configured! It's time to reboot now. Have fun!"
