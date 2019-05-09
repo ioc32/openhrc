a60694a36a9d8933e4491cb62cfd3cb63ea2c1ed
Iñigo Ortiz de Urbina <inigo@infornografia.net>
2019-05-09 21:53:28 +0200

    Fix warning

--- configure.sh
+++ configure.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "Running stage1..."
-ansible-playbook -e @local-vars.yml stage1.yml
+ansible-playbook -i inventory -e @local-vars.yml stage1.yml
 
 echo "Running stage2..."
-ansible-playbook -e @local-vars.yml stage2.yml
+ansible-playbook -i inventory -e @local-vars.yml stage2.yml
 
 echo "System configured! It's time to reboot now. Have fun!"
