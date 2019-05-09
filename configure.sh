964e502719090c5ad2aefc24f494709d841e9208
Iñigo Ortiz de Urbina <inigo@infornografia.net>
2019-05-09 23:04:58 +0200

    One Playbook To Rule Them All (TM)

--- configure.sh
+++ configure.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-echo "Running stage1..."
-ansible-playbook -i inventory -e @local-vars.yml stage1.yml
-
-echo "Running stage2..."
-ansible-playbook -i inventory -e @local-vars.yml stage2.yml
+echo "Running playbook..."
+ansible-playbook -i inventory -e @local-vars.yml main.yml
 
 echo "System configured! It's time to reboot now. Have fun!"
